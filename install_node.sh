2df110cc555943ad95a42f93b09171d387615746
Elbert Oh <oh.applications@gmail.com>
2021-04-19 12:15:45 -0400

    Updated install_node script to ask for version

--- install_node.sh
+++ install_node.sh
@@ -2,11 +2,15 @@
 
 source std_lib.sh
 
+echo Enter version:
+
+read version
+
 print_msg "Adding Node repository..."
 
 type curl > /dev/null 2>&1 || sudo apt install curl
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL "https://deb.nodesource.com/setup_$version.x" | sudo -E bash -
 
 handle_error "Node repository could not be added"
 
