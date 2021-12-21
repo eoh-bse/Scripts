048808146769ef8f0eb4170298a6a1b49d7d285c
Elbert Oh <oh.applications@gmail.com>
2021-12-21 11:53:15 -0500

    Update dotnet script

--- install_dotnet.sh
+++ install_dotnet.sh
@@ -2,7 +2,10 @@
 
 source std_lib.sh
 
-wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -P ~/Downloads/
+echo "Enter dotnet version to install:"
+read version
+
+wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -P ~/Downloads/
 handle_error "Failed to download packages-microsoft-prod.deb"
 
 echo "${YELLOW}Authenticate for installing the .deb package${NO_COLOR}"
@@ -18,5 +21,5 @@ handle_error "Failed to install apt-transport-https"
 sudo apt update
 handle_error "Failed to apt update"
 
-sudo apt install dotnet-sdk-2.2
+sudo apt install "dotnet-sdk-$version"
 handle_error "Failed to install dotnet-sdk-2.2"
