4b36eb17cd0b1889733accce2f065c701e4a73cb
Elbert Oh <ellokri89@gmail.com>
2022-12-06 23:39:16 -0500

    fix go, nvim install scripts (#2)

--- common/install_latest_neovim.sh
+++ common/install_latest_neovim.sh
@@ -1,6 +1,6 @@
 # !/usr/bin/env bash
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
 
 sudo mv -f ./nvim.appimage /usr/local/bin/nvim
