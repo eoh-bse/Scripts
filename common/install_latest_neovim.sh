ff3a2caa6305527efaa64204e34ffe3dfcefd271
Elbert Oh <ellokri89@gmail.com>
2024-04-25 19:07:40 -0400

    minor fixes on neovim and ssh keygen scripts (#8)

--- common/install_latest_neovim.sh
+++ common/install_latest_neovim.sh
@@ -1,4 +1,4 @@
-# !/usr/bin/env bash
+#!/usr/bin/env bash
 
 curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
--- fedora/generate_ssh_ed25519.sh
+++ fedora/generate_ssh_ed25519.sh
@@ -6,8 +6,4 @@ read email
 
 ssh-keygen -t ed25519 -C "$email"
 
-type xclip >/dev/null 2>&1 || sudo apt install xclip
-
-echo "Type 'xclip -sel clip < your public ssh key location' to copy the public key and paste it your gitlab account"
-
-echo "Go to https://gitlab.com/users/sign_in"
+type wl-copy >/dev/null 2>&1 || sudo dnf install wl-clipboard
