180fb550e6d1086160d11f0eba6a68dc37f97d48
Elbert Oh <ellokri89@gmail.com>
2024-06-13 08:31:10 -0400

    remove unused scripts + update tmux config (#9)

--- fedora/disable_mac_keyboard_fb.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
-sudo dracut -v -f
--- fedora/install_jdk.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Installing JDK..."
-
-sudo dnf install java-11-openjdk-devel.x86_64
